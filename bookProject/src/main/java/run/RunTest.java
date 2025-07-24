64b57c3bcb7696ffe239ce1d0132e1a6e81041ec
Kotaewoo <rhxodn011020@naver.com>
2025-07-24 18:20:19 +0900

    초기 설정

---
+++ bookProject/src/main/java/run/RunTest.java
@@ -0,0 +1,30 @@
+package run;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import org.junit.Test;
+
+import model.entity.book;
+import util.DBUtil;
+
+public class RunTest {
+	// select
+	 @Test
+	public void step01() {
+		EntityManager em = DBUtil.getEntityManager();
+		/*
+		 * Object em.find(Emp01.class, 1) -> Object em.find(Object byte명, pk): pk 값으로
+		 * select하는 기능 - pk 타입에 맞춰 검색 필수
+		 */
+		System.out.println(em.find(book.class, 1));
+		System.out.println(em.find(book.class, 2));
+		System.out.println(em.find(book.class, 3));
+
+		em.close();
+		em = null;
+	}
+
+}
---
+++ bookProject/src/main/java/util/DBUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import org.junit.Test;
+
+public class DBUtil {
+	private static EntityManagerFactory emf;
+	static {
+		emf = Persistence.createEntityManagerFactory("dbinfo");
+	}
+
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+
+	public static void close() {
+		if (emf != null) {
+			emf.close();
+			emf = null;
+		}
+	}
+
+	@Test
+	public void test() {
+		System.out.println(getEntityManager());
+	}
+}
