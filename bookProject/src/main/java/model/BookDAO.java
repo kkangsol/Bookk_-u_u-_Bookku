635b5ae70ec167ef1fe412bc170759238486b0e7
KIM DONGMIN <ehdals5387@naver.com>
2025-07-25 10:23:34 +0900

    feat: DAO 기능별 추가 (#1)

--- bookProject/src/main/java/model/BookDAO.java
+++ bookProject/src/main/java/model/BookDAO.java
@@ -2,6 +2,8 @@ package model;
 
 import model.entity.book;
 import javax.persistence.*;
+
+import java.sql.SQLException;
 import java.util.List;
 
 public class BookDAO {
@@ -23,4 +25,50 @@ public class BookDAO {
 
         return list;
     }
+    
+    public List<book> findTitle(String title) throws SQLException {
+        EntityManager em = emf.createEntityManager();
+        List<book> list = null;
+
+        try {
+            list = em.createQuery("SELECT b FROM book b WHERE b.title = :title", book.class)
+                     .setParameter("title", title)
+                     .getResultList();
+        } finally {
+            em.close();
+        }
+
+        return list;
+    }
+    
+    public List<book> findCategory(String category) throws SQLException{
+    	EntityManager em = emf.createEntityManager();
+        List<book> list = null;
+        
+        try {
+        	list = em.createQuery("SELECT b FROM book b WHERE b.category = :category", book.class)
+        			.setParameter("category", category)
+        			.getResultList();
+        } finally {
+        	em.close();
+        }
+        
+        return list;
+    }
+    
+    public List<book> findAuthor(String author) throws SQLException{
+    	EntityManager em = emf.createEntityManager();
+        List<book> list = null;
+        
+        try {
+        	list = em.createQuery("SELECT b FROM book b WHERE b.author = :author", book.class)
+        			.setParameter("category", author)
+        			.getResultList();
+        } finally {
+        	em.close();
+        }
+        
+        return list;
+    }
+
 }
